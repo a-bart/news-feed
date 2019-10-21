711d1338ba9a11dc4b1075db2f8be1c24cc9f8dd
Aleksey Bartashevich <alexey.barton@gmail.com>
2019-10-21 11:34:39 +0300

    feat: use dotenv to load env vars

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
   api: {
-    url: 'https://newsapi.org/v2',
-    key: 'b17828702c284971b7257ecb95dd40c0',
+    url: process.env.API_URL,
+    key: process.env.API_KEY,
   },
   memoization: {
     amount: 5,
--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
-const withStylus = require('@zeit/next-stylus');
-// const withSass = require('@zeit/next-sass');
+/* eslint no-param-reassign: 0 */
+/* eslint import/no-extraneous-dependencies: 0 */
+require('dotenv').config();
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
+const stylus = require('@zeit/next-stylus');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = withStylus({});
+module.exports = withPlugins([
+  [stylus, {}],
+], {
+  webpack: (config) => {
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true,
+      }),
+    ];
+
+    return config;
+  },
+});
